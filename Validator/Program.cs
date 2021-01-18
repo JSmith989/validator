ff5561ef45fd24e4d5ce593299837b8a6bdcc137
Jordan Smith <jordansmith989@gmail.com>
2021-01-17 21:32:39 -0600

    first part of validator

--- Validator/Program.cs
+++ Validator/Program.cs
@@ -6,7 +6,19 @@ namespace Validator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a pin(number between 4-8 digits long)");
+            var pin = Console.ReadLine();
+            var isNumeric = int.TryParse(pin, out _);
+            if (!int.TryParse(pin, out _))
+            {
+                Console.WriteLine("Only Numbers");
+
+            }
+            else if (pin.Length >= 4 && pin.Length <= 8)
+            {
+                Console.WriteLine("Pin Valid");
+            };
+
         }
     }
 }
